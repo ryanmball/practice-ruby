5f33bced67167569c26a70b64db9a8cb5308919f
ryan <ball.ryan.rb@gmail.com>
2021-04-28 13:22:42 -0600

    adds ryan's climbing and outdoor store and 3 products

---
+++ store_item.rb
@@ -0,0 +1,43 @@
+puts "Welcome to Ryan's climbing and outdoor store!"
+# product1 = {name: "BD Camalot 0.75", color: "green", use: "trad", price: 69.95}
+# product2 = {name: "Mammut Infinity Dry 9.5mm", color: "pink", use: "all", price: 280.00}
+# product3 = {:name => "BD Hotforged Hybrid QD", :color => "blue", :use => "sport", :price => 17.95}
+
+class Product
+
+  def initialize(name, color, use, price)
+    @name = name
+    @color = color
+    @use = use
+    @price = price
+  end
+
+  attr_accessor :name, :color, :use, :price
+
+  def name
+    @name
+  end
+
+  def price
+    @price
+  end
+
+  def price=(new_price)
+    @price = new_price
+  end
+
+  def print_info
+    puts "Product Name: #{name}, Color: #{color}, Uses: #{use}, Price: #{price}"
+  end
+
+end
+
+product1 = Product.new("BD Camalot 0.75", "green", "trad", 69.95)
+product2 = Product.new("Mammut Infinity Dry 9.5mm", "pink", "all", 280.00)
+product3 = Product.new("BD Hotforged Hybrid QD", "blue", "sport", 17.95)
+
+product1.print_info
+product2.print_info
+product3.print_info
+product1.price = 59.95
+product1.print_info
